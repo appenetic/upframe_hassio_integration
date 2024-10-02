bd55db5a603519c098ab6b04a39db3e3bdef4ca8
Appenetic <appenetic@icloud.com>
2024-10-02 21:02:57 +0200

    FIX: Intergration initialisation

--- custom_components/upframe_controller/__init__.py
+++ custom_components/upframe_controller/__init__.py
@@ -14,16 +14,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up display control from a config entry."""
     hass.data.setdefault(DOMAIN, {})
 
-    # Store the configured URL in hass.data
+    # Store config entry
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    # Forward setup to platforms
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "switch")
-    )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, "switch")
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
